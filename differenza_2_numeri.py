f6686cad7dbfa0cd4a1b4652c916eda0b47ba3f6
unknown <niccolo.panciroli@gmail.com>
2021-01-06 16:38:49 +0100

    aggiungo l'esercizio numero 14

---
+++ differenza_2_numeri.py
@@ -0,0 +1,7 @@
+a = int(input("Inserisci il primo numero"))
+b = int(input("Inserisci il secondo numero"))
+
+if a*b>10:
+    print("La differenza tra a e b è: ", a-b)
+else:
+    print("La somma tra a e b è: ", a+b)
