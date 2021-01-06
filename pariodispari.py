902a18ec118634a3694237ee813dac53a81d06bd
unknown <niccolo.panciroli@gmail.com>
2021-01-06 17:10:20 +0100

    modifica al file

--- pariodispari.py
+++ pariodispari.py
@@ -1,3 +1,5 @@
+print("Verifica se un numero è pari o dispari (un numero è pari quando il resto della divisione intera per 0 è uguale a 2")
+print()
 numero= int(input("Inserisci un numero da verificare"))
 if numero%2 == 0:
     print("Il numero ",numero, " è pari")
